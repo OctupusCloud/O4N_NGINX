a7ef3b651232f5f28d677ed6df66a3e382e45fd8
Ed <edgardo.scrimaglia@gmail.com>
2024-08-26 08:42:56 -0300

    start cron

--- nginxConfAuto/scriptNginx.py
+++ nginxConfAuto/scriptNginx.py
@@ -6,14 +6,15 @@ load_dotenv()
 
 WEB_SECURITY = os.getenv('WEB_SECURITY')
 WEB_SECURITY = WEB_SECURITY.upper()
+BACKEND = os.getenv('BACKEND')
 
 if WEB_SECURITY == 'TRUE':
-	
-	templateElection = 'nginx_security.template'
+
+	templateElection = BACKEND.lower()+"_"+'nginx_security.template'
 
 else:
 
-	templateElection = 'nginx_no_security.template'
+	templateElection = BACKEND.lower()+"_"'nginx_no_security.template'
 
 template = open(templateElection, 'r+')
 readTemplate = template.read()
@@ -21,7 +22,7 @@ template.close()
 
 jtemplate = jinja2.Template(readTemplate)
 
-renderJ2 = jtemplate.render(DOMAIN_SERVER=os.getenv('DOMAIN_SERVER'), IP_SERVER=os.getenv('IP_SERVER'))
+renderJ2 = jtemplate.render(DOMAIN_SERVER=os.getenv('DOMAIN_SERVER'), IP_SERVER=os.getenv('IP_SERVER'), BACKEND=os.getenv('BACKEND'), PORT=os.getenv('PORT'))
 
 config = open('nginx.conf', 'w')
 config.write(renderJ2)
